6794aad30cbb2093bd0860720bf97b1cc5473269
Dmitry Tinkov <mrfredyon@gmail.com>
2024-01-18 20:48:39 +0300

    user path

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -64,14 +64,4 @@ routeUser.patch('/:id', async (req, res) => {
   }
 });
 
-routeUser.patch('/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const body = req.body;
-    const data = await updateUserPath(id, body);
-    buildResponse(200, data, res);
-  } catch (error) {
-    buildResponse(404, error.message, res);
-  }
-});
 module.exports = routeUser;
