1b2b822860c2a0732879d71ffc65ed1d460d059e
Dmitry Tinkov <mrfredyon@gmail.com>
2024-01-19 19:40:31 +0300

    hashing password

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -1,9 +1,13 @@
 const { createUserApiDB, getUserByEmailDB } = require('../repository/api.repository');
+const bcrypt = require('bcrypt');
+
+const saltround = 3;
 
 async function createUserApi(name, surname, email, pwd) {
   const findEmail = await getUserByEmailDB(email);
   if (findEmail) throw new Error('USER IS ALREDY EXIST');
-  const data = await createUserApiDB(name, surname, email, pwd);
+  const hashPwd = await bcrypt.hash(pwd, saltround);
+  const data = await createUserApiDB(name, surname, email, hashPwd);
   if (!data.length) throw new Error('data is empty');
   return data;
 }
